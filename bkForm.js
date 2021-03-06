82eff10189b0260a21fe15abd5278863947721bb
ucs-hkg <79682073+ucs-hkg@users.noreply.github.com>
2021-03-06 13:45:56 +0800

    Update bkForm.js

--- bkForm.js
+++ bkForm.js
@@ -9,3 +9,23 @@ function testGS(){
 }
 
 document.getElementById("btn").addEventListener("click",testGS);
+
+function addGS(){
+    const url = "https://script.google.com/macros/s/AKfycbx809pH5h_GZI0yaBcBvZeqtvsG-Gp60jFbqDm9P6bM3Fs9gr9G/exec"
+
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"John"}) // body data type must match "Content-Type" header
+    });
+        
+}
+
+document.getElementById("btn2").addEventListener("click",addGS);
